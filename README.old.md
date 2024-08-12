241fc26e214a256ff9260408ee714d0c3803c0eb
Gerardo Filho <gerardo_filho95@hotmail..com>
2024-08-12 12:10:09 -0300

    inicio do projeto

---
+++ README.old.md
@@ -0,0 +1,3 @@
+# magelas-project
+
+https://www.figma.com/design/QjhnQITTU2hv44rZniEfqL/Casulo-Redesign?node-id=0-1
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="magelas-brothers App-logo">Magela's Brothers</h1>
+    </div>
+  );
+}
+
+export default App;
